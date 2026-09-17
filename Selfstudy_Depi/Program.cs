0851e1746ea527726c68f59385299bd54e5dae99
ahmed <ahmed@DESKTOP-7TG39AK>
2026-09-17 19:08:57 +0300

    Add project files.

---
+++ Selfstudy_Depi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Selfstudy_Depi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
